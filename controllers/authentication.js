e413bba3e1f468d95dff14e564194be9325d6166
Martijn Wip <flyingwip@gmail.com>
2020-10-30 18:29:31 +0100

    add signup

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,12 +1,27 @@
 const models = require('../models');
 
-exports.signup = function(req, res, next){
+exports.signup = async function(req, res, next){
+
+    const email = req.body.email
+    const password = req.body.password
     
+    // first check if user already exists 
+    let user = await models.User.findOne( { where: { email: email } } )
+        .catch((error)=>{
+            console.log( '=========> ', error)
+        })
     
-    models.User.findAll().then((result) => {
-        console.log(result)
-        // response.render('user/index', { title: 'User list', users: result } );
-    });
+    // if user exist return an error    
+    if(user){
+        // unprocessable entity
+        return res.status(422).send({error: 'Email in use'})
+    }    
     
-    res.send({succes:'true'})
+    // create a new user 
+    user = await models.User.create({ email: email, password: email })
+        .catch((error=>{
+            return next(error)
+        }))
+
+    res.json({user})
 }
